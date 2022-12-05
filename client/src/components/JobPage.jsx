9cb83253008feacfbc414c327a3163b67fcd6579
Yisroel Arnson <yisroelrnsn@gmail.com>
2022-12-05 11:55:38 -0500

    displayed all the data for a job on a job page

--- client/src/components/JobPage.jsx
+++ client/src/components/JobPage.jsx
@@ -1,15 +1,20 @@
 import { React, useState } from "react";
 
 export default function JobPage(props) {
+  const [jobFormOpen, setJobFormOpen] = useState(false)
   const linen = JSON.parse(props.job[5]);
   const napkins = JSON.parse(props.job[6]);
   const linenValues = Object.values(linen);
   const napkinsValues = Object.values(napkins);
-
+  const jobDate = new Date(props.job[2]);
   const getLinenNameFromId = (id) => {
     for (let i = 0; i < props.linenList.length; i++) {
       if (props.linenList[i][1] === id) {
-        return props.linenList[i][0];
+        return [
+          props.linenList[i][0],
+          props.linenList[i][6],
+          props.linenList[i][7],
+        ];
       }
     }
   };
@@ -24,22 +29,39 @@ export default function JobPage(props) {
   return (
     <div className="job-page-modal">
       <div className="job-page-content">
-        <h2 onClick={() => props.setJobPageOpen(false)}>x</h2>
-        <h1>{props.job[1]}</h1>
-        {linenValues.map((val) => {
-          return (
-            <h4>
-              {val["count"]} - {getLinenNameFromId(val["unique_id"])}
-            </h4>
-          );
-        })}
-        {napkinsValues.map((val) => {
-          return (
-            <h4 className="napkin-text">
-              {val["count"]} - {getNapkinNameFromId(val["unique_id"])}
-            </h4>
-          );
-        })}
+        <div className="job-page-top-bar">
+          <h1>Name of client: {props.job[1]}</h1>
+          <div className="top-bar-right">
+            <div className="job-page-edit-button" onClick={}>Edit</div>
+            <h2
+              className="exit-button"
+              onClick={() => props.setJobPageOpen(false)}
+            >
+              x
+            </h2>
+          </div>
+        </div>
+        <h2>Date: {jobDate.toDateString()}</h2>
+        <h2>Location: {props.job[3]}</h2>
+        <div className="linen-table">
+          {linenValues.map((val, index) => {
+            const linenData = getLinenNameFromId(val["unique_id"]);
+            return (
+              <h4 key={index}>
+                {val["count"]} - {linenData[0]} - ${linenData[1]}
+              </h4>
+            );
+          })}
+          {napkinsValues.map((val) => {
+            return (
+              <h4 className="napkin-text">
+                {val["count"]} - {getNapkinNameFromId(val["unique_id"])}
+              </h4>
+            );
+          })}
+        </div>
+
+        <p>{props.job[9]}</p>
       </div>
     </div>
   );
